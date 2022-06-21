f9d4cfd9ffc9848423898b073157333c2527b0f8
Hiram Kumado <hiramkumado@gmail.com>
2022-06-21 21:38:41 +0000

    Added button functionality to the develop branch

--- claim_app.py
+++ claim_app.py
@@ -2,6 +2,7 @@ from tkinter import *
 from tkinter import ttk
 import pandas as pd
 from PIL import ImageTk
+from tkinter import messagebox
 # db = open('info.txt', 'r')
 # db = open('info.txt', 'a')
 # vals = db.write(f'{user}, {password}\n')
@@ -12,6 +13,70 @@ window = Tk()
 window.title('IAS REQUEST FORM')
 window.geometry('600x600+450+50')
 window.resizable(0, 0)
+
+request_date = StringVar()
+team_leader = StringVar()
+station_name = StringVar()
+work_process1 = IntVar()
+work_process2 = IntVar()
+item = IntVar()
+item_quantity = IntVar()
+item_description = StringVar()
+item_cost = IntVar()
+total_cost = IntVar()
+refrence_no = IntVar()
+payment_type = StringVar()
+payment_ref = StringVar()
+authorised_by = StringVar()
+signature = StringVar()
+authorised_date = StringVar()
+approved_lbl_en = StringVar()
+approval_signature = StringVar()
+approval_date_ent = StringVar()
+# ============ Functions ====================
+
+
+def reset():
+    request_date_entry.delete(0, END)
+    team_leader_entry.delete(0, END)
+    station_name_entry.delete(0, END)
+    work_process1.get()
+    work_process2.get()
+    item_entry.delete(0, END)
+    quantity_entry.delete(0, END)
+    description_entry.delete(0, END)
+    unit_cost_entry.delete(0, END)
+    total_cost_entry.delete(0, END)
+    refrence_no_entry.delete(0, END)
+    payment_type_entry.delete(0, END)
+    payment_ref_entry.delete(0, END)
+    authorised_by_entry.delete(0, END)
+    approval_signature_entry.delete(0, END)
+    autorised_date_entry.delete(0, END)
+    approved_lbl_entry.delete(0, END)
+    approval_date_entry.delete(0, END)
+    signature_entry.delete(0, END)
+
+
+def cancel():
+    if ".!button4":
+        result = messagebox.askyesno(
+            "CANCEL!", "Click Yes to cancel or No to continue")
+        if result == True:
+            window.destroy()
+        else:
+            pass
+
+
+def extra():
+    item_entry.delete(0, END)
+    quantity_entry.delete(0, END)
+    description_entry.delete(0, END)
+    unit_cost_entry.delete(0, END)
+    total_cost_entry.delete(0, END)
+
+
+# ===========================================
 logo = ImageTk.PhotoImage(file="image\ias.png")
 logo_img = Label(window, bd=0, image=logo)
 logo_img.place(y=0, x=0)
@@ -25,113 +90,134 @@ seperation.place(x=160, y=74)
 
 date_lbl = Label(window, font=('impact', 12),
                  text=('DATE.'), foreground='black').place(x=415, y=105)
-request_date_entry = ttk.Entry(window, font=('Helvetica', 15, 'bold'))
+request_date_entry = ttk.Entry(
+    window, textvariable=request_date, font=('Helvetica', 15, 'bold'))
 request_date_entry.place(x=455, y=105, width=140)
 request_date_entry.configure(background='white')
 
 team_leader_lbl = Label(window, font=('impact', 12),
                         text=('TEAM LEADER:'), foreground='black').place(x=10, y=105)
-team_leader_entry = ttk.Entry(window, font=('Helvetica', 15, 'bold'))
+team_leader_entry = ttk.Entry(
+    window, textvariable=team_leader, font=('Helvetica', 15, 'bold'))
 team_leader_entry.place(x=100, y=105)
 team_leader_entry.configure(background='white')
 
 preventative_lbl = Label(window, font=('impact', 12),
                          text=('STATION NAME:'), foreground='black').place(x=10, y=200)
-station_name_entry = ttk.Entry(window, font=('Helvetica', 15, 'bold'))
+station_name_entry = ttk.Entry(
+    window, textvariable=station_name, font=('Helvetica', 15, 'bold'))
 station_name_entry.place(x=120, y=200)
 preventative_lbl = ttk.Label(window, font=('impact', 12),
                              text=('TYPE OF MAINTENANCE ACTIVITY:'), foreground='black').place(x=10, y=150)
 
-corrective_check_box = ttk.Checkbutton(window, text='CORRECTIVE')
+corrective_check_box = ttk.Checkbutton(
+    window, variable=work_process1, text='CORRECTIVE')
 corrective_check_box.place(x=220, y=150)
-corrective_check_box = ttk.Checkbutton(window, text='PREVENTATIVE')
-corrective_check_box.place(x=330, y=150)
+preventive_check_box = ttk.Checkbutton(
+    window, variable=work_process2, text='PREVENTATIVE')
+preventive_check_box.place(x=330, y=150)
+corrective_check_box.bind("<Button-1>")
+preventive_check_box.bind("<Button-1>")
 
 item_lbl = Label(window, font=('impact', 12),
                  text=('ITEM'), foreground='black').place(x=10, y=260)
-item_entry = ttk.Entry(window, font=('Helvetica', 15, 'bold'))
+item_entry = ttk.Entry(window, textvariable=item,
+                       font=('Helvetica', 15, 'bold'))
 item_entry.place(x=5, y=290, width=50)
 item_entry.configure(background='white')
 
 quantity_lbl = Label(window, font=('impact', 12),
                      text=('QUANTITY'), foreground='black').place(x=75, y=260)
-quantity_entry = ttk.Entry(window, font=('Helvetica', 15, 'bold'))
+quantity_entry = ttk.Entry(
+    window, textvariable=item_quantity, font=('Helvetica', 15, 'bold'))
 quantity_entry.place(x=60, y=290, width=100)
 quantity_entry.configure(background='white')
 # ========================================================
 descpription_lbl = Label(window, font=('impact', 12),
                          text=('DESCRIPTION'), foreground='black').place(x=190, y=260)
-description_entry = ttk.Entry(window, font=('Helvetica', 15, 'bold'))
+description_entry = ttk.Entry(
+    window, textvariable=item_description, font=('Helvetica', 15, 'bold'))
 description_entry.place(width=140, x=165, y=290)
 description_entry.configure(background='white')
 
 unit_cost_lbl = Label(window, font=('impact', 12),
                       text=('UNIT COST'), foreground='black').place(x=340, y=260)
-unit_cost_entry = ttk.Entry(window, font=('Helvetica', 15, 'bold'))
+unit_cost_entry = ttk.Entry(
+    window, textvariable=item_cost, font=('Helvetica', 15, 'bold'))
 unit_cost_entry.place(width=140, x=310, y=290)
 unit_cost_entry.configure(background='white')
 
 total_cost_lbl = Label(window, font=('impact', 12),
                        text=('TOTAL COST'), foreground='black').place(x=480, y=260)
-total_cost_entry = ttk.Entry(window, font=('Helvetica', 15, 'bold'))
+total_cost_entry = ttk.Entry(
+    window, textvariable=total_cost, font=('Helvetica', 15, 'bold'))
 total_cost_entry.place(width=140, x=455, y=290)
 total_cost_entry.configure(background='white')
 # ================== Reference column ====================
 refrence_no_lbl = Label(window, font=('impact', 12),
                         text=('Ref No'), foreground='black').place(x=35, y=350)
-refrence_no_entry = ttk.Entry(window, font=('Helvetica', 15, 'bold'))
+refrence_no_entry = ttk.Entry(
+    window, textvariable=refrence_no, font=('Helvetica', 15, 'bold'))
 refrence_no_entry.place(width=50, x=85, y=350)
 
 payment_type_lbl = Label(window, font=('impact', 12),
                          text=('Payment Type'), foreground='black').place(x=160, y=350)
-payment_type_entry = ttk.Entry(window, font=('Helvetica', 15, 'bold'))
+payment_type_entry = ttk.Entry(
+    window, textvariable=payment_type, font=('Helvetica', 15, 'bold'))
 payment_type_entry.place(width=100, x=260, y=350)
 
 payment_ref_lbl = Label(window, font=('impact', 12),
                         text=('Payment Ref'), foreground='black').place(x=385, y=350)
-payment_ref_entry = ttk.Entry(window, font=('Helvetica', 15, 'bold'))
+payment_ref_entry = ttk.Entry(
+    window, textvariable=payment_ref, font=('Helvetica', 15, 'bold'))
 payment_ref_entry.place(width=100, x=475, y=350)
 # ===================== Authorised column =================
 authorised_by_lbl = Label(window, font=('impact', 12),
                           text=('Authorised by'), foreground='black').place(x=25, y=390)
-authorised_by_entry = ttk.Entry(window, font=('Helvetica', 15, 'bold'))
+authorised_by_entry = ttk.Entry(
+    window, textvariable=authorised_by, font=('Helvetica', 15, 'bold'))
 authorised_by_entry.place(width=100, x=135, y=390)
 
 signature_lbl = Label(window, font=('impact', 12),
                       text=('Signature'), foreground='black').place(x=245, y=390)
-signature_entry = ttk.Entry(window, font=('Helvetica', 15, 'bold'))
+signature_entry = ttk.Entry(
+    window, textvariable=signature, font=('Helvetica', 15, 'bold'))
 signature_entry.place(width=100, x=315, y=390)
 
 autorised_date_lbl = ttk.Label(window, font=('impact', 12),
                                text=('Date'), foreground='black').place(x=425, y=390)
-autorised_date_entry = ttk.Entry(window, font=('Helvetica', 15, 'bold'))
+autorised_date_entry = ttk.Entry(
+    window, textvariable=authorised_date, font=('Helvetica', 15, 'bold'))
 autorised_date_entry.place(width=100, x=475, y=390)
 # =================== Approved column=====================
 approved_lbl = ttk.Label(window, font=('impact', 12),
                          text=('Approved by'), foreground='black').place(x=25, y=430)
-approved_lbl_entry = ttk.Entry(window, font=('Helvetica', 15, 'bold'))
+approved_lbl_entry = ttk.Entry(
+    window, textvariable=approved_lbl_en, font=('Helvetica', 15, 'bold'))
 approved_lbl_entry.place(width=100, x=135, y=430)
 
 approval_signature_lbl = ttk.Label(window, font=('impact', 12),
                                    text=('Signature'), foreground='black').place(x=245, y=430)
-approval_signature_entry = ttk.Entry(window, font=('Helvetica', 15, 'bold'))
+approval_signature_entry = ttk.Entry(
+    window, textvariable=approval_signature, font=('Helvetica', 15, 'bold'))
 approval_signature_entry.place(width=100, x=315, y=430)
 
 approval_date_lbl = ttk.Label(window, font=('impact', 12),
                               text=('Date'), foreground='black').place(x=425, y=430)
-approval_date_entry = ttk.Entry(window, font=('Helvetica', 15, 'bold'))
+approval_date_entry = ttk.Entry(
+    window, textvariable=approval_date_ent, font=('Helvetica', 15, 'bold'))
 approval_date_entry.place(width=100, x=475, y=430)
 # ================ Buttons For Action=================
 submit_btn = ttk.Button(window, text='SUBMIT', cursor='hand2')
 submit_btn.place(x=500, y=500)
 
-extra_btn = ttk.Button(window, text='EXTRA', cursor='hand2')
+extra_btn = ttk.Button(window, command=extra, text='EXTRA', cursor='hand2')
 extra_btn.place(x=415, y=500)
 
-reset_btn = ttk.Button(window, text='RESET', cursor='hand2')
+reset_btn = ttk.Button(window, command=reset, text='RESET', cursor='hand2')
 reset_btn.place(x=330, y=500)
 
-cancel_btn = ttk.Button(window, text='CANCEL', cursor='hand2')
+cancel_btn = ttk.Button(window, command=cancel, text='CANCEL', cursor='hand2')
 cancel_btn.place(x=245, y=500)
 
 # ================= Seperation Frame================
